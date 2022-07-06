305b303ca20831af2638888da21a7609415558c9
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2022-07-06 15:38:29 +0200

    first commit

---
+++ exact_laws/math/derivation.py
@@ -0,0 +1,117 @@
+import numpy as np
+
+def cdiff(tab, length_case=1, dirr=0, precision=4, period=True, point=False):
+    """
+    Return the differential of 'tab' according to a quantity whose length is 'length_case' whith a centre method \n
+    Caution 1: if 'tab' has more than one dimension, don't forget to indicate the direction ('dirr') of the differential (default=0) \n
+    Caution 2: periodical edge effects by default (period=True) \n
+    Caution 3: 'length_case' is the length of a case \n
+    précision ordre 2 (default if not period): need to know the one-step-left and right values \n
+    précision ordre 4 (default): need to know the two-step-left and right values \n
+    Remark : np.roll shift the table to the right (the end comes to the begining)
+    """
+    if len(np.shape(tab)) == 1:
+        dirr = 0
+    if length_case == 0:
+        return np.zeros(np.shape(tab))
+    tab = np.array(tab)
+    if precision == 4:
+        if not point:
+            result = (
+                             np.roll(tab, 2, axis=int(dirr))
+                             + 8 * np.roll(tab, -1, axis=int(dirr))
+                             - 8 * np.roll(tab, 1, axis=int(dirr))
+                             - np.roll(tab, -2, axis=int(dirr))
+                     ) / (12 * length_case)
+            if not period:
+                if dirr == 0:
+                    result[0] = (tab[1] - tab[0]) / length_case
+                    result[1] = (tab[2] - tab[0]) / (2 * length_case)
+                    result[-2] = (tab[-1] - tab[-3]) / (2 * length_case)
+                    result[-1] = (tab[-1] - tab[-2]) / length_case
+                elif dirr == 1:
+                    result[:, 0] = (tab[:, 1] - tab[:, 0]) / length_case
+                    result[:, 1] = (tab[:, 2] - tab[:, 0]) / (2 * length_case)
+                    result[:, -2] = (tab[:, -1] - tab[:, -3]) / (2 * length_case)
+                    result[:, -1] = (tab[:, -1] - tab[:, -2]) / length_case
+                elif dirr == 2:
+                    result[:, :, 0] = (tab[:, :, 1] - tab[:, :, 0]) / length_case
+                    result[:, :, 1] = (tab[:, :, 2] - tab[:, :, 0]) / (2 * length_case)
+                    result[:, :, -2] = (tab[:, :, -1] - tab[:, :, -3]) / (2 * length_case)
+                    result[:, :, -1] = (tab[:, :, -1] - tab[:, :, -2]) / length_case
+        else:
+            result = (tab[-1] + 8 * tab[1] - 8 * tab[-2] - tab[0]) / (12 * length_case)
+    elif precision == 2:
+        if not point:
+            result = (np.roll(tab, -1, axis=int(dirr)) - np.roll(tab, 1, axis=int(dirr))) / (2 * length_case)
+            if not period:
+                if dirr == 0:
+                    result[0] = (tab[1] - tab[0]) / length_case
+                    result[-1] = (tab[-1] - tab[-2]) / length_case
+                elif dirr == 1:
+                    result[:, 0] = (tab[:, 1] - tab[:, 0]) / length_case
+                    result[:, -1] = (tab[:, -1] - tab[:, -2]) / length_case
+                elif dirr == 2:
+                    result[:, :, 0] = (tab[:, :, 1] - tab[:, :, 0]) / length_case
+                    result[:, :, -1] = (tab[:, :, -1] - tab[:, :, -2]) / length_case
+        else:
+            result = (tab[-1] - tab[0]) / (2 * length_case)
+    return result
+
+
+def div(tab_vec, case_vec=[None], precision=4, period=True):
+    """
+    Return the divergence of 'tab_vec' by a vectorial quantity whose resolution is 'case_vec' (cartesian coordinates)
+    Caution 1: 'tab_vec' has to have the same number of dimensions than the 'case_vec' length
+    Caution 2: 'case_vec' is the length of the cases in each directions
+    for 'precision' and 'period', see 'cdiff' function description
+    """
+    if case_vec[0] is None:
+        case_vec = np.ones(len(tab_vec), dtype=int)
+    return np.sum(
+        [cdiff(tab_vec[i], case_vec[i], i, precision, period) for i in range(len(case_vec))],
+        axis=0,
+    )
+
+
+def rot_gen(tab_vec, case_vec=[None], precision=4, period=True):
+    if case_vec[0] is None:
+        case_vec = np.ones(len(tab_vec), dtype=int)
+    for i in range(len(case_vec)):
+        if i == 0:
+            A = cdiff(tab_vec[2], case_vec[1], 1, precision, period)
+            B = cdiff(tab_vec[1], case_vec[2], 2, precision, period)
+        elif i == 1:
+            A = cdiff(tab_vec[0], case_vec[2], 2, precision, period)
+            B = cdiff(tab_vec[2], case_vec[0], 0, precision, period)
+        elif i == 2:
+            A = cdiff(tab_vec[1], case_vec[0], 0, precision, period)
+            B = cdiff(tab_vec[0], case_vec[1], 1, precision, period)
+        yield A - B
+
+
+def rot(tab_vec, case_vec=[None], precision=4, period=True):
+    """
+    Return the rotational of 'tab_vec' by a vectorial quantity whose resolution is 'case_vec'
+    Caution 1: 'tab_vec' has to have the same number of dimensions than the 'case_vec' length
+    Caution 2: 'case_vec' is the length of the cases in each directions
+    for 'precision' and 'period', see 'cdiff' function description
+    """
+    return list(rot_gen(tab_vec, case_vec, precision, period))
+
+
+def grad_gen(tab, case_vec=[None], precision=4, period=True):
+    if case_vec[0] is None:
+        case_vec = np.ones(len(np.shape(tab)), dtype=int)
+    for i in range(len(case_vec)):
+        yield cdiff(tab, case_vec[i], i, precision, period)
+
+
+def grad(tab, case_vec=[None], precision=4, period=True):
+    """
+    Return the gradient of 'tab' by a vectorial quantity whose resolution is 'case_vec'
+    Caution 1: 'tab' has to have the same number of dimensions than the 'case_vec' length
+    Caution 2: 'case_vec' is the length of the cases in each directions
+    for 'precision' and 'period', see 'cdiff' function description
+    """
+    return list(grad_gen(tab, case_vec, precision, period))
