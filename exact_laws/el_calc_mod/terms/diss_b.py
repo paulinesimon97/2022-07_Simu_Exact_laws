c6a4c2712be85886f0eb14ea264b804455a9adbb
Pauline Simon <posylone@gmail.com>
2023-01-26 08:57:45 +0100

    correction error in dissb
    version "25/01/2023"

--- exact_laws/el_calc_mod/terms/diss_b.py
+++ exact_laws/el_calc_mod/terms/diss_b.py
@@ -9,13 +9,13 @@ class DissB(DissV):
     def __init__(self):
         DissV.__init__(self)
 
-    def calc(self, vector: List[int], cube_size: List[int], rho, vx, vy, vz, hdmx, hdmy, hdmz, **kwarg
+    def calc(self, vector: List[int], cube_size: List[int], rho, bx, by, bz, hdmx, hdmy, hdmz, **kwarg
         ) -> List[float]:
         return calc_source_with_numba(
-            calc_in_point_with_sympy, *vector, *cube_size, rho, vx, vy, vz, hdmx, hdmy, hdmz)
+            calc_in_point_with_sympy, *vector, *cube_size, rho, bx, by, bz, hdmx, hdmy, hdmz)
 
     def variables(self) -> List[str]:
-        return ["hdm", "v", "rho"]
+        return ["hdm", "b", "rho"]
 
 
 def load():
